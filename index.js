777d899ea074536a7c4e94066b77c95df6f248e1
KiishiO <79492192+kiishi-0@users.noreply.github.com>
2023-02-01 10:46:18 +0000

    done with this project

--- index.js
+++ index.js
@@ -1,9 +1,69 @@
 var number = document.querySelectorAll(".drum")
-var audio = new Audio('sounds/tom-2.mp3')
-for (var i = 0; i < number.length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", (e) => {
-    console.log(e.target.innerHTML)
-  });
+
+
+
+const makeSound = (key) => {
+  switch (key) {
+    case 'w':
+      var tom1 = new Audio('sounds/tom-1.mp3');
+      tom1.play();
+      break;
+    case 'a':
+      var tom2 = new Audio('sounds/tom-2.mp3');
+      tom2.play();
+      break;
+    case 's':
+      var tom3 = new Audio('sounds/tom-3.mp3');
+      tom3.play();
+      break;
+    case 'd':
+      var tom4 = new Audio('sounds/tom-4.mp3');
+      tom4.play();
+      break;
+
+    case 'j':
+      var snare = new Audio('sounds/snare.mp3');
+      snare.play();
+      break;
+
+    case 'k':
+      var crash = new Audio('sounds/crash.mp3');
+      crash.play();
+      break;
+
+    case 'l':
+      var kick = new Audio('sounds/kick-bass.mp3');
+      kick.play();
+      break;
+
+    default:
+
+
+  }
 }
 
-// audio.play();
+for (var i = 0; i < number.length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", (e) => {
+    var ans = e.target.innerHTML;
+    console.log(ans)
+    makeSound(e.target.innerHTML)
+    buttonAnimation(e.target.innerHTML)
+  }
+  );
+
+}
+
+
+document.addEventListener("keydown", (e) => {
+  makeSound(e.key)
+  buttonAnimation(e.key)
+})
+
+const buttonAnimation = (currentKey) => {
+  var activeButton = document.querySelector('.' + currentKey);
+
+  activeButton.classList.add("pressed");
+  setTimeout(() => {
+    activeButton.classList.remove('pressed')
+  }), 3000
+};
