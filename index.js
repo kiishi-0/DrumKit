9db759cf6556cb463f5d8e8e6ce8ca605ef26f00
Iteoluwakiishi Oyewole <Iteoluwakiishi.Oyewole@unitedcapitalplcgroup.com>
2023-02-01 09:48:57 +0100

    Fixed the issue with the targeting inner html

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var number = document.querySelectorAll(".drum")
 var audio = new Audio('sounds/tom-2.mp3')
 for (var i = 0; i < number.length; i++) {
-  document.querySelectorAll(".drum")[i].addEventListener("click", () => {
-    console.log(this.innerHTML)
+  document.querySelectorAll(".drum")[i].addEventListener("click", (e) => {
+    console.log(e.target.innerHTML)
   });
 }
 
-audio.play();
+// audio.play();
